fb00e9d589dc0831af6ce26a025bf63b87e2de5a
sowjanyamanugula <manugulasowjanya@gmail.com>
2017-01-10 12:20:58 +0530

    Frontend Devlopment Session5 Assignment3 code
    Frontend Devlopment Session5 Assignment3 code

---
+++ js/app.js
@@ -0,0 +1,11 @@
+ var Employee={
+        Name:"Johny",
+        age:25,
+        salary:"6 lakhs p/a",
+        address:{
+                  city:"Hyderabad",
+                  State:"Telangana",
+                  Pincode:"500084"
+        }
+ };
+        console.log(Employee);
